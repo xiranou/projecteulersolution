7e268e7a0721b8ad299addfbd523d69d425a7d00
xiranou <xiranou@gmail.com>
2015-01-26 22:54:03 -0500

    refactor problem two solution

--- problem_two/2.rb
+++ problem_two/2.rb
@@ -1,14 +1,16 @@
-def generate_fibo(term, fibos=[1,2], i=0)
+def generate_fibos(term, fibos=[1,2], i=0)
   new_fibo = fibos[i] + fibos[i+1]
   return fibos if new_fibo >= term
   fibos << new_fibo
-  generate_fibo(term, fibos, i+1)
+  generate_fibos(term, fibos, i+1)
+end
+
+def get_even_fibos(term)
+  generate_fibos(term).select { |fibo| fibo.even? }
 end
 
 def do_magic(term)
-  generate_fibo(term).select do |fibo|
-    fibo.even?
-  end.inject(:+)
+  get_even_fibos(term).inject(:+)
 end
 
 p do_magic(4000000)
